5cf1a99c040dd9f4c91ff5abd8fd46d7c1259d22
Henddher Pedroza <henddher.pedroza@gmail.com>
2025-07-05 12:14:07 -0500

    Multiplayer Local Master https://boardgame.io/documentation/#/multiplayer?id=local-master.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
+import { Local } from "boardgame.io/multiplayer";
 import { Client } from "boardgame.io/react";
 import { TicTacToe } from "./Game";
 import { TicTacToeBoard } from "./Board";
 
-const App = Client({ game: TicTacToe, board: TicTacToeBoard });
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: Local(),
+});
+
+const App = () => (
+  <div>
+    <TicTacToeClient playerID="0" />
+    <TicTacToeClient playerID="1" />
+  </div>
+);
 
 export default App;
