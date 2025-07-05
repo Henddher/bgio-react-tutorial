d800cee76afb12fd1bc785204624bd37e7afcb4b
Henddher Pedroza <henddher.pedroza@gmail.com>
2025-07-05 12:18:42 -0500

    Multiplayer Remote Master https://boardgame.io/documentation/#/multiplayer?id=remote-master
    Fails:
    
    ❯ npm run serve
    
    > bgio-react-tutorial@0.1.0 serve
    > node src/server.js
    
    /Users/henddher/Documents/github/bgio-react-tutorial/src/Game.js:1
    import { INVALID_MOVE } from "boardgame.io/core";
    ^^^^^^
    
    SyntaxError: Cannot use import statement outside a module
        at wrapSafe (node:internal/modules/cjs/loader:1378:20)
        at Module._compile (node:internal/modules/cjs/loader:1428:41)
        at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)
        at Module.load (node:internal/modules/cjs/loader:1288:32)
        at Module._load (node:internal/modules/cjs/loader:1104:12)
        at Module.require (node:internal/modules/cjs/loader:1311:19)
        at require (node:internal/modules/helpers:179:18)
        at Object.<anonymous>
    (/Users/henddher/Documents/github/bgio-react-tutorial/src/server.js:3:23)
        at Module._compile (node:internal/modules/cjs/loader:1469:14)
        at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)
    
    Node.js v20.18.0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Local } from "boardgame.io/multiplayer";
 import { Client } from "boardgame.io/react";
 import { TicTacToe } from "./Game";
 import { TicTacToeBoard } from "./Board";
+import { SocketIO } from "boardgame.io/multiplayer";
 
 const TicTacToeClient = Client({
   game: TicTacToe,
   board: TicTacToeBoard,
-  multiplayer: Local(),
+  multiplayer: SocketIO({ server: "localhost:8000" }),
 });
 
 const App = () => (
